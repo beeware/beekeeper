270d2fba4700cbf7913f76d2eea80b9e8ffed9dd
Russell Keith-Magee <russell@keith-magee.com>
2017-06-25 10:54:12 +0800

    Tweaked display of avatars in admin.

--- github/admin.py
+++ github/admin.py
@@ -4,26 +4,33 @@ from django.utils.safestring import mark_safe
 from .models import User, Repository, PullRequest
 
 
-def avatar(github_user):
-    return mark_safe('<img src="%s" alt="Github avatar">' % github_user.avatar_url)
-avatar.short_description = 'Avatar'
-
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['login', avatar, 'user']
+    list_display = ['login', 'avatar', 'user']
     list_filter = ['user_type']
     raw_id_fields = ['user']
+
+    def avatar(self, user):
+        return mark_safe('<img src="%s" style="width: 32px" alt="Github avatar">' % user.avatar_url)
+    avatar.short_description = 'Avatar'
 
 
 @admin.register(Repository)
 class RepositoryAdmin(admin.ModelAdmin):
-    list_display = ['owner', 'name', 'description']
+    list_display = ['owner', 'avatar', 'name', 'description']
     raw_id_fields = ['owner',]
+
+    def avatar(self, repo):
+        return mark_safe('<img src="%s" style="width: 32px" alt="Github avatar">' % repo.owner.avatar_url)
+    avatar.short_description = 'Avatar'
 
 
 @admin.register(PullRequest)
 class PullRequestAdmin(admin.ModelAdmin):
-    list_display = ['number', 'repository', 'user', 'state']
+    list_display = ['number', 'repository', 'user', 'avatar', 'state']
     list_filter = ['state']
     raw_id_fields = ['repository', 'user']
+
+    def avatar(self, pr):
+        return mark_safe('<img src="%s" style="width: 32px" alt="Github avatar">' % pr.user.avatar_url)
+    avatar.short_description = 'Avatar'
