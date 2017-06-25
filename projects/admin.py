481183f53615e43cb41b6e7f2189808edb70e47e
Russell Keith-Magee <russell@keith-magee.com>
2017-06-25 21:14:12 +0800

    Added admin actions to approve projects.

--- projects/admin.py
+++ projects/admin.py
@@ -2,12 +2,33 @@ from django.contrib import admin
 
 from .models import Project, Build
 
+def approve(modeladmin, request, queryset):
+    for project in queryset:
+        project.status = Project.STATUS_ACTIVE
+        messages.info(request, 'Approving %s for build' % project)
+approve.short_description = "Approve for build"
+
+
+def attic(modeladmin, request, queryset):
+    for project in queryset:
+        project.status = Project.STATUS_ATTIC
+        messages.info(request, 'Moving %s to the attic' % project)
+attic.short_description = "Move to attic"
+
+
+def ignore(modeladmin, request, queryset):
+    for project in queryset:
+        project.status = Project.STATUS_IGNORE
+        messages.info(request, 'Ignoring %s' % project)
+ignore.short_description = "Ignore"
+
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['repository', 'status']
     list_filter = ['status']
     raw_id_fields = ['repository']
+    actions = [approve, attic, ignore]
 
 
 @admin.register(Build)
