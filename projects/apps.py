117d382798b77cab64fa00108d558a6597a2a469
Russell Keith-Magee <russell@keith-magee.com>
2017-06-26 11:01:15 +0800

    Register signal handler for commits.

--- projects/apps.py
+++ projects/apps.py
@@ -7,8 +7,10 @@ class ProjectsConfig(AppConfig):
     def ready(self):
         from django.db.models import signals as django
         from github import signals as github
-        from github.models import Repository, PullRequest
+        from github.models import Repository, Commit, PullRequest
         from .signals import new_project, new_build
 
         django.post_save.connect(new_project, sender=Repository)
+
+        github.new_build.connect(new_build, sender=Commit)
         github.new_build.connect(new_build, sender=PullRequest)
