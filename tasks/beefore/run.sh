32d6295b7fcfa66b6923f28f4ebb12a103eb498d
Russell Keith-Magee <russell@keith-magee.com>
2017-07-10 19:20:29 +0800

    Corrected quoting in beefore build file.

--- tasks/beefore/run.sh
+++ tasks/beefore/run.sh
@@ -5,8 +5,8 @@ curl -s -L -u $GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN https://github.com/$GITHUB_O
 unzip code.zip
 
 echo "Python version=`python --version`"
-echo "Node version=`node --version`
-echo "NPM version=`npm --version`
+echo "Node version=`node --version`"
+echo "NPM version=`npm --version`"
 
 # Run checks
 echo beefore -u $GITHUB_USERNAME -r $GITHUB_OWNER/$GITHUB_PROJECT_NAME -p $GITHUB_PR_NUMBER -c $SHA $TASK $GITHUB_PROJECT_NAME-$SHA
