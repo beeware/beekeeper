562e579965237ba9d06c5e85c861210760459d10
Russell Keith-Magee <russell@keith-magee.com>
2017-07-13 13:37:41 +0800

    Improved debugging output of beefore run script.

--- tasks/beefore/run.sh
+++ tasks/beefore/run.sh
@@ -4,10 +4,22 @@
 curl -s -L -u $GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN https://github.com/$GITHUB_OWNER/$GITHUB_PROJECT_NAME/archive/$SHA.zip -o code.zip
 unzip code.zip
 
+echo
 echo "Python version=`python --version`"
 echo "Node version=`node --version`"
 echo "NPM version=`npm --version`"
+echo
 
 # Run checks
-echo beefore -u $GITHUB_USERNAME -r $GITHUB_OWNER/$GITHUB_PROJECT_NAME -p $GITHUB_PR_NUMBER -c $SHA $TASK $GITHUB_PROJECT_NAME-$SHA
-beefore -u $GITHUB_USERNAME -r $GITHUB_OWNER/$GITHUB_PROJECT_NAME -p $GITHUB_PR_NUMBER -c $SHA $TASK $GITHUB_PROJECT_NAME-$SHA
+echo beefore
+echo "    --username $GITHUB_USERNAME"
+echo "    --repository $GITHUB_OWNER/$GITHUB_PROJECT_NAME"
+echo "    --pull-request $GITHUB_PR_NUMBER"
+echo "    --commit $SHA"
+echo "    $TASK $GITHUB_PROJECT_NAME-$SHA"
+
+beefore --username $GITHUB_USERNAME \
+    --repository $GITHUB_OWNER/$GITHUB_PROJECT_NAME \
+    --pull-request $GITHUB_PR_NUMBER \
+    --commit $SHA \
+    $TASK $GITHUB_PROJECT_NAME-$SHA
